f6a98dfff383b421b15cc203b6ab09a4ced4ccc3
lzaxel <lzaxelmail@gmail.com>
2022-05-29 21:27:30 +0300

    Stylized manga add form

--- manga/forms.py
+++ manga/forms.py
@@ -14,17 +14,18 @@ class MangaAddForm(forms.ModelForm):
         fields = ['name', 'original_name', 'description', 'series',
                     'author', 'language', 'tags', 'uploader', 'zip']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'input add-manga-form__input',
+            'name': forms.TextInput(attrs={'class': 'manga-form__input',
                                             'placeholder': 'Название'}),
-            'original_name': forms.TextInput(attrs={'class': 'input add-manga-form__input',
+            'original_name': forms.TextInput(attrs={'class': 'manga-form__input',
                                             'placeholder': 'Оригинальное Название'}),
-            'description': forms.Textarea(attrs={'class': 'input add-manga-form__input', 'rows': 5,
+            'description': forms.Textarea(attrs={'class': 'manga-form__input', 'rows': 5,
                                             'placeholder': 'Описание'}),
-            'series': forms.TextInput(attrs={'class': 'input add-manga-form__input',
+            'series': forms.TextInput(attrs={'class': 'manga-form__input',
                                             'placeholder': 'Серия'}),
-            'author': forms.TextInput(attrs={'class': 'input add-manga-form__input',
+            'author': forms.TextInput(attrs={'class': 'manga-form__input',
                                             'placeholder': 'Автор манги'}),
-            'language': forms.Select(attrs={'class': 'input add-manga-form__input'}),
-            'tags': forms.SelectMultiple(attrs={'class': 'input add-manga-form__choice'}),
-            'zip': forms.FileInput(attrs={'class': 'file-input add-manga-form__file-input'})
+            'language': forms.Select(attrs={'class': 'manga-form__input'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'manga-form__input'}),
+            'uploader': forms.Select(attrs={'class': 'manga-form__input'}),
+            'zip': forms.FileInput(attrs={'class': 'manga-form__file'})
         }
