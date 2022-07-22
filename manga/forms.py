21d9ef11273f6fada1797bf969cf5af78b0c9187
lzaxel <lzaxelmail@gmail.com>
2022-07-23 00:52:03 +0300

    Refactored css classes
    Buttons, inputs, textareas, selects, checkboxes
    Brackets of django-debug-toolbar)

--- manga/forms.py
+++ manga/forms.py
@@ -12,32 +12,32 @@ class MangaAddForm(forms.ModelForm):
         fields = ['name', 'original_name', 'description', 'series',
                     'author', 'language', 'tags', 'zip', 'nsfw']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form__input',
+            'name': forms.TextInput(attrs={'class': 'input input--inner',
                                             'placeholder': 'Название'}),
-            'original_name': forms.TextInput(attrs={'class': 'form__input',
+            'original_name': forms.TextInput(attrs={'class': 'input input--inner',
                                             'placeholder': 'Оригинальное Название'}),
-            'description': forms.Textarea(attrs={'class': 'form__input', 'rows': 5,
+            'description': forms.Textarea(attrs={'class': 'textarea', 'rows': 5,
                                             'placeholder': 'Описание'}),
-            'series': forms.TextInput(attrs={'class': 'form__input',
+            'series': forms.TextInput(attrs={'class': 'input input--inner',
                                             'placeholder': 'Серия'}),
-            'author': forms.TextInput(attrs={'class': 'form__input',
+            'author': forms.TextInput(attrs={'class': 'input input--inner',
                                             'placeholder': 'Автор манги'}),
-            'language': forms.Select(attrs={'class': 'form__input'}),
-            'tags': forms.SelectMultiple(attrs={'class': 'form__input'}),
-            'nsfw': forms.CheckboxInput(attrs={'class': 'check__input'}),
-            'zip': forms.FileInput(attrs={'class': 'form__file'})
+            'language': forms.Select(attrs={'class': 'input input--inner'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'select'}),
+            'nsfw': forms.CheckboxInput(attrs={'class': 'check-box'}),
+            'zip': forms.FileInput(attrs={'class': 'file'})
         }
 
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(
-        attrs={'class': 'form__input', 'placeholder': 'Логин'}
+        attrs={'class': 'input input--inner', 'placeholder': 'Логин'}
     ))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(
-        attrs={'class': 'form__input', 'placeholder': 'Пароль'}
+        attrs={'class': 'input input--inner', 'placeholder': 'Пароль'}
     ))
     password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput(
-        attrs={'class': 'form__input', 'placeholder': 'Повторите пароль'}
+        attrs={'class': 'input input--inner', 'placeholder': 'Повторите пароль'}
     ))
 
     class Meta:
@@ -47,8 +47,18 @@ class SignUpForm(UserCreationForm):
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(
-        attrs={'class': 'form__input', 'placeholder': 'Логин'}
+        attrs={'class': 'input input--inner', 'placeholder': 'Логин'}
     ))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(
-        attrs={'class': 'form__input', 'placeholder': 'Пароль'}
+        attrs={'class': 'input input--inner', 'placeholder': 'Пароль'}
     ))
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
+        widgets = {
+            'text': forms.Textarea(attrs={'class': 'textarea',
+                                            'name': 'text', 'cols':'15', 'rows':'6'}),
+        }
--- website/settings.py
+++ website/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'manga.apps.MangaConfig',
-    "debug_toolbar",
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
